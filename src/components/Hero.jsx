14e4be63e3dfa4a87d7b58758fee461ce6f031e7
jjcanell77 <jjcanell@yahoo.com>
2024-01-22 17:17:22 -0600

    App Completed
    added last components.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 // import React from "react";
+import Button from "./Button"
 
 export default function Hero(){
     return (
@@ -14,11 +15,9 @@ export default function Hero(){
             <p className="text-sm md:text-base font-light">I hereby acknowledgement that I may become <span className="text-blue-400 font-medium">unbelievably
             swolenormous</span> and accept all risks of becoming the local <span className="text-blue-400 font-medium">mass
             montrosity</span>, afflicted with severe body dismorphia, unable to fit through doors.</p>
-            <button className="px-8 py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200">
-                <p>
-                    Accept & Begin
-                </p>
-            </button>
+            <Button func={()=>{
+                window.location.href= "#generate"
+            }} text = "Accept & Begin"/>
         </div>
     )
 }
